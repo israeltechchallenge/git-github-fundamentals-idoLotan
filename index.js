ee93e59d570750251dff20f461e3a51d4a06f92f
idoLotan <91139751+idoLotan@users.noreply.github.com>
2022-05-16 19:06:10 +0300

    milestone 6-7

--- index.js
+++ index.js
@@ -1,45 +1,58 @@
-let isBtn = document.getElementById("isBtn");
+document.getElementById("spinner").style.display = "none";
+document.getElementById("spinner-down").style.display = "none";
+isBtn = document.getElementById("isBtn");
+let inputNum = document.getElementById("inputNum");
+let fibonacciResults = 0;
+let storedResults = "";
 
-let GITHUB_URL = "";
+// fibonaci calculator
 
-function sendInput() {
-  let inputNum = document.getElementById("inputNum");
-  let stringinput = `http://localhost:5050/fibonacci/${String(inputNum.value)}`;
-  console.log(stringinput);
+function fibonaciNumber(firstNum, secoundNum, thirdNum, x, y) {
+  firstNum = 0;
+  secoundNum = 1;
+  thirdNum = 0;
+  x = inputNum.value - 1;
+  cleanUpCalculator();
+  if (inputNum.value > 50) {
+    return upperLimit();
+  }
+  if (inputNum.value == 42) {
+    return meaningOfLifeEror();
+  }
+  for (i = 0; i < x; i++) {
+    thirdNum = firstNum + secoundNum;
+    firstNum = secoundNum;
+    secoundNum = thirdNum;
+  }
 
-  fetch(stringinput)
-    .then(function (response) {
-      return response.json();
-    })
-
-    .then(function (data) {
-      // get my name and profile image  from my github profile
-
-      function showResults() {
-        console.log(inputNum.value);
-        document.getElementById("output").innerText = data.result;
-      }
-      showResults();
-    })
-    .catch((error) => {
-      if (inputNum.value == 42) {
-        document.getElementById("output").innerText =
-          "Server Error: 42 is the meaning of life";
-        document.getElementById("output").classList.add("meaning-of-life-eror");
-      } else if (inputNum.value > 50) {
-        document.getElementById("inputNum").classList.add("red-alert"),
-          (document.getElementById("output").innerText =
-            "Can't be larger then 50"),
-          document.getElementById("output").classList.add("red-alert-prop");
-      } else {
-        document.getElementById("output").innerText = "general eror";
-      }
-    });
+  y = thirdNum;
+  document.getElementById("output").innerText = y;
+  return y;
 }
 
-document.getElementById("spinner").style.display = "block";
-isBtn.addEventListener("click", sendInput);
+// display an eror when user put in value greater then 50
 
+function upperLimit() {
+  document.getElementById("inputNum").classList.add("red-alert"),
+    (document.getElementById("output").innerText = "Can't be larger then 50"),
+    document.getElementById("output").classList.add("red-alert-prop");
+}
+
+function cleanUpCalculator() {
+  document.getElementById("inputNum").classList.remove("red-alert");
+  document.getElementById("output").classList.remove("red-alert-prop");
+  document.getElementById("output").classList.remove("meaning-of-life-eror");
+}
+
+function meaningOfLifeEror() {
+  document.getElementById("output").innerText =
+    "Server Error: 42 is the meaning of life";
+  document.getElementById("output").classList.add("meaning-of-life-eror");
+}
+
+// spinner display for the displayed results (will be change soon)
+
+// spinner display for stored results(will be change soon)
 function initialState() {
   document.getElementById("spinner").style.display = "none";
   document.getElementById("output").style.display = "block";
@@ -48,17 +61,115 @@ function initialState() {
 function finalState() {
   document.getElementById("spinner").style.display = "block";
   document.getElementById("output").style.display = "none";
+  myVar = setTimeout(initialState, 1000);
   fibonacciResults = fibonaciNumber();
 }
 
-initialState();
-
 isBtn.addEventListener("click", finalState);
 
-// card animation
+let storedCalculation = [];
 
-// function showResults() {
-//   document.getElementById("output").innerText = fibonaciNumber(inputNum);
+function createString() {
+  let cube = {
+    inputX: "",
+    outputY: "",
+    date: "",
+  };
+  let date = new Date().toLocaleString("en-US");
+  cube.inputX = inputNum.value;
+  cube.outputY = fibonacciResults.toString();
+  cube.date = date;
+  storedCalculation.push({ cube });
+}
+
+isBtn.addEventListener("click", createString);
+
+function showResults() {
+  let pastResults = document.getElementById("pastResults");
+  clearResults();
+  for (let i = 0; i < storedCalculation.length; i++) {
+    let para = document.createElement(`p`);
+    let calcValues = Object.values(storedCalculation[i].cube);
+    let xVal = calcValues[0].bold();
+    let yVal = calcValues[1].bold();
+    let dateVal = calcValues[2];
+
+    para.innerHTML =
+      "The Fibonnaci Of " +
+      xVal +
+      " is " +
+      yVal +
+      " Calculated at: " +
+      dateVal +
+      " GMT+0200 (Israel Standard Time)";
+    pastResults.appendChild(para);
+  }
+}
+
+function clearResults() {
+  const myNode = document.getElementById("pastResults");
+  myNode.innerHTML = "";
+}
+
+function initialStore() {
+  storedResults = showResults();
+  document.getElementById("spinner-down").style.display = "none";
+}
+
+function finalStore() {
+  document.getElementById("spinner-down").style.display = "block";
+  myVar = setTimeout(initialStore, 1000);
+}
+
+let results = document.getElementById("results");
+results.addEventListener("click", finalStore);
+
+function selectorSort() {
+  // let sortedArray = storedCalculation[0].cube.inputX.sort(function (a, b) {
+  //   return a - b;
+  // });
+  // console.log(sortedArray);
+
+  storedCalculation.sort((a, b) => {
+    return a.cube.inputX - b.cube.inputX;
+  });
+
+  let selector = document.getElementById("selector");
+  if (selector.value == "number Asc") {
+    console.log(storedCalculation);
+  } else {
+    console.log("ff");
+  }
+
+  // for (let i = 0; i < array.length; i++) {
+  //   storedCalculation[i].cube;
+
+  //   });
+}
+
+let select = document.getElementById("selector");
+select.addEventListener("click", selectorSort);
+
+// let stringo = "hello".bold();
+
+// document.getElementById("usedLang").innerHTML = stringo + "gg";
+
+// function createString() {
+//   date = new Date().toLocaleString("en-US");
+//   storedCalculation.push(
+//     `The Fibonnaci Of ${inputNum.value} is ${fibonacciResults}. Calculated at: ${date} GMT+0200 (Israel Standard Time) `
+//   );
 // }
 
-// isBtn.addEventListener("click", showResults);
+// isBtn.addEventListener("click", createString);
+
+// let results = document.getElementById("results");
+// results.addEventListener("click", showResults);
+
+// function showResults() {
+//   for (let i = 0; i < storedCalculation.length; i++) {
+//     let para = document.createElement(`p`);
+//     para.innerText = storedCalculation[i];
+//     pastResults.appendChild(para);
+//   }
+// }
